deb8f8586c8756f993e81c9c36bc58f06234040b
Raphael Titsworth-Morin <raphael@ephemerecreative.ca>
2019-10-22 10:59:12 -0700

    label outputs

--- .chui/index.ts
+++ .chui/index.ts
@@ -1,13 +1,9 @@
 import * as cluster from "./cluster";
-import {Config} from "@chuistack/chui-lib";
-
-
-(async () => {
-    const config = await Config.loadCurrentConfig();
-    console.log("CONFIGUUURATION: ", config);
-})();
-
 
 const clusterOutput = cluster.install();
+
+/***********************
+ * OUTPUTS             *
+ ***********************/
 
 export const clusterName = clusterOutput.cluster.name;
